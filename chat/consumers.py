2204ce253bdaf3f82d93840ff198a4b4ff0db521
Nam Hee KIM <nami200284@snu.ac.kr>
2024-05-10 16:48:58 +0900

    add annotation

--- chat/consumers.py
+++ chat/consumers.py
@@ -5,6 +5,23 @@ from channels.generic.websocket import WebsocketConsumer
 
 
 class ChatConsumer(WebsocketConsumer):
+    """
+    Represents a WebSocket consumer for handling chat functionality.
+
+    This consumer is responsible for connecting to a specific chat room,
+    receiving and sending messages within the room.
+
+    Attributes:
+        room_name (str): The name of the chat room.
+        room_group_name (str): The group name for the chat room.
+
+    Methods:
+        connect(): Connects to the chat room and adds the consumer to the room group.
+        disconnect(close_code): Disconnects from the chat room and removes the consumer from the room group.
+        receive(text_data): Receives a message from the WebSocket and sends it to the room group.
+        chat_message(event): Receives a message from the room group and sends it to the WebSocket.
+    """
+
     def connect(self):
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.room_group_name = f"chat_{self.room_name}"
